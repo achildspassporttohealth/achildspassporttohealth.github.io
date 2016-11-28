d760f312929dc14d351ff6bd266c93ac096bcdd8
Vince Seavello <register@seavello.com>
2016-11-27 21:37:49 -0700

    add new extended access token. should be good until Jan 26, 2017

--- scripts/fbaccess.js
+++ scripts/fbaccess.js
@@ -1,3 +1,7 @@
 var pageid = "1687150331514073";
-var accesstoken = "EAAIlZAx7c9gkBAOEWqnT6hVRrURKiChvl88OFCzVoaTHUk7CmU9LoKWXDf8USW6cFDfsf1qZCRs2MH8ZBrEtfCZAU6prCoXuVD7HK862dZBcLslFFByYkno0woYBbFgfdHzuiqYCVKZAaGnZCJJzPbNSnXZBczhJGFQZD";
-var expires = "5184000";
+//var accesstoken = "EAAIlZAx7c9gkBAOEWqnT6hVRrURKiChvl88OFCzVoaTHUk7CmU9LoKWXDf8USW6cFDfsf1qZCRs2MH8ZBrEtfCZAU6prCoXuVD7HK862dZBcLslFFByYkno0woYBbFgfdHzuiqYCVKZAaGnZCJJzPbNSnXZBczhJGFQZD";
+//var expires = "5184000";
+
+// expires January 26th, 2017
+var accesstoken = "EAAIi6nfwTlwBAOJtWgagB0ZADe12nZA34ybbicZCg584a265CX23DpQr4QEsDdiqca1C5FiP8ZBZC4SRzc1j2Dh9e4FsC4gks7iyzqwn9JjDXz7vnPrlDNDrmykQ26l6Kl2jz2FlLHZBvW4cupdkVt5NG0OPuHzbAZD";
+var expires = "1485484695";
