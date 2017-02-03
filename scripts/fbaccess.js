3c925f71f45057ae4369fee601f52bfad97f3a14
Vince Seavello <register@seavello.com>
2017-02-03 12:53:49 -0700

    update access token

--- scripts/fbaccess.js
+++ scripts/fbaccess.js
@@ -3,5 +3,9 @@ var pageid = "1687150331514073";
 //var expires = "5184000";
 
 // expires January 26th, 2017
-var accesstoken = "EAAIi6nfwTlwBAJGZAG8S2CJ2yzGWG2gZCdP9ua4laI8VnAcyX4ytrUHsB2r9mnY0XToCzRD0Hd0AytZAFMZBZBXXpbTbb4obLZCUWhgdFFxrPfZAhTcK1gkmNzqxuZCwL8tPS9Sp3AbmJ0kjKvOqHjpTOiZCKPgrjWN4ZD";
+//var accesstoken = "EAAIi6nfwTlwBAJGZAG8S2CJ2yzGWG2gZCdP9ua4laI8VnAcyX4ytrUHsB2r9mnY0XToCzRD0Hd0AytZAFMZBZBXXpbTbb4obLZCUWhgdFFxrPfZAhTcK1gkmNzqxuZCwL8tPS9Sp3AbmJ0kjKvOqHjpTOiZCKPgrjWN4ZD";
+//var expires = "1486583909";
+
+// expires April 4th, 2017
+var accesstoken = "EAAIlZAx7c9gkBAI1BaZChl9LEmEoA92upmzXYtZCZCaAZBn1edCyh491KvMdwyx4Huz7fZAwErZCsKqZBFMPIJ1Tf2ZAWsZCOHZAaLpSafhY0yuxNuuGhPUpzos7DPWLXcehZCRjhZAgtaDJCmEIXRHHLPwvqL25WBpXzVqwZD";
 var expires = "1486583909";
